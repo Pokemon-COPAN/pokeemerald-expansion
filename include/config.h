5a0097d78070dc4766b93b5db8d3a7f3cb859940
Copansko <125827702+Copansko@users.noreply.github.com>
2023-03-23 21:14:27 +0100

    Added !
    !

--- include/config.h
+++ include/config.h
@@ -1,7 +1,7 @@
 #ifndef GUARD_CONFIG_H
 #define GUARD_CONFIG_H
 
-// In the Generation 3 games, Asserts were used in various debug builds.
+// In the Generation 3 games, Asserts were used in various debug builds!
 // Ruby/Sapphire and Emerald do not have these asserts while Fire Red
 // still has them in the ROM. This is because the developers forgot
 // to define NDEBUG before release, however this has been changed as
