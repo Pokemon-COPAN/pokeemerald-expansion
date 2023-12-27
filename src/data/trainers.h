be916a33eb9197022044b1ff2e8834020f53d35f
Copansko <bamboospammer@gmail.com>
2023-12-27 17:27:54 +0100

    Jeroen now has pokemon

--- src/data/trainers.h
+++ src/data/trainers.h
@@ -10260,4 +10260,17 @@ const struct Trainer gTrainers[] = {
         .aiFlags = 0,
         .party = NO_ITEM_DEFAULT_MOVES(sParty_MayLinkPlaceholder),
     },
+	
+	[TRAINER_JEROEN] =
+    {
+        .trainerClass = TRAINER_CLASS_LEADER,
+        .encounterMusic_gender = TRAINER_ENCOUNTER_MUSIC_MALE,
+        .trainerPic = TRAINER_PIC_LEADER_BRAWLY,
+        .trainerName = _("JEROEN"),
+        .items = {},
+        .doubleBattle = FALSE,
+        .aiFlags = AI_FLAG_CHECK_BAD_MOVE | AI_FLAG_TRY_TO_FAINT | AI_FLAG_CHECK_VIABILITY,
+        .party = ITEM_CUSTOM_MOVES(sParty_Jeroen),
+    },
 };
+
