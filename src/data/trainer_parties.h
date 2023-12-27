6390c2d166db9c07a5fa38d5ad1f9def0019fc54
Copansko <bamboospammer@gmail.com>
2023-12-27 17:27:54 +0100

    Jeroen now has pokemon

--- src/data/trainer_parties.h
+++ src/data/trainer_parties.h
@@ -11804,3 +11804,48 @@ static const struct TrainerMon sParty_MayLinkPlaceholder[] = {
     .species = SPECIES_KYOGRE,
     }
 };
+
+static const struct TrainerMonItemCustomMoves sParty_Jeroen[] = {
+    {
+    .iv = 0,
+    .lvl = 13,
+    .species = SPECIES_NOSEPASS,
+    .heldItem = ITEM_NONE,
+    .moves = {MOVE_THUNDER_WAVE, MOVE_ROCK_TOMB, MOVE_NONE, MOVE_NONE}
+    },
+    {
+    .iv = 0,
+    .lvl = 12,
+    .species = SPECIES_LILEEP,
+    .heldItem = GRIP_CLAW,
+    .moves = {MOVE_INGRAIN, MOVE_WRAP, MOVE_ASTONISH, MOVE_CONFUSE_RAY}
+    },
+    {
+    .iv = 0,
+    .lvl = 12,
+    .species = SPECIES_GEODUDE_ALOLAN,
+    .heldItem = ITEM_NONE,
+    .moves = {MOVE_CHARGE, MOVE_SPARK, MOVE_NONE, MOVE_NONE}
+    },
+    {
+    .iv = 0,
+    .lvl = 12,
+    .species = SPECIES_OMANYTE,
+    .heldItem = ITEM_NONE,
+    .moves = {MOVE_ROLLOUT, MOVE_DEFENSE_CURL, MOVE_NONE, MOVE_NONE}
+    },
+    {
+    .iv = 0,
+    .lvl = 12,
+    .species = SPECIES_UNOWN,
+    .heldItem = ITEM_NONE,
+    .moves = {MOVE_ANCIENT_POWER, MOVE_NONE, MOVE_NONE, MOVE_NONE}
+    },
+    {
+    .iv = 0,
+    .lvl = 14,
+    .species = SPECIES_ANORITH,
+    .heldItem = ITEM_ORAN_BERRY_BERRY,
+    .moves = {MOVE_FURY_CUTTER, MOVE_ROCK_TOMB, MOVE_CUT, MOVE_NONE}
+    }
+};
